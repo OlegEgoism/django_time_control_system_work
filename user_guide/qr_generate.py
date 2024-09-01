1b1ce0801326015d57aa73062fd459adaaa4a903
OlegEgoism <olegpustovalov220@gmail.com>
2024-09-01 20:47:23 +0300

    add requirements.txt

--- user_guide/qr_generate.py
+++ user_guide/qr_generate.py
@@ -1,20 +1,3 @@
-# import qrcode
-#
-# data = "Пустовалов Олег"
-# print('Данные QRCode -->', data)
-#
-# qr = qrcode.QRCode(
-#     version=2,
-#     box_size=30,
-#     border=5,
-#     error_correction=qrcode.constants.ERROR_CORRECT_Q,
-# )
-# qr.add_data(data)
-# qr.make(fit=True)
-# img = qr.make_image(fill_color="black", back_color="white")
-# img.save(f"media/photo_qr/{data}.png")
-
-
 import os
 import qrcode
 import transliterate  # Нужно установить пакет transliterate
