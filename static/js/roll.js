04f6fcd5ab45eaf20858a8d34610f79ca89988e5
OlegEgoism <olegpustovalov220@gmail.com>
2024-11-03 20:38:36 +0300

    Рабочий вариант сайта

--- static/js/roll.js
+++ static/js/roll.js
@@ -1,4 +1,4 @@
-// user_info.html
+// user_info.html -- Биография
 function toggleBio() {
     const shortBio = document.getElementById("bio-short");
     const fullBio = document.getElementById("bio-full");
@@ -16,29 +16,47 @@ function toggleBio() {
 }
 
 
-// user_info.html
+// user_info.html -- Участие в проектах
 function toggleDescriptionProject(counter) {
-    const shortDesc = document.getElementById(`description-short-${counter}`);
-    const fullDesc = document.getElementById(`description-full-${counter}`);
+    const shortDescPro = document.getElementById(`description-short-${counter}`);
+    const fullDescPro = document.getElementById(`description-full-${counter}`);
     const toggleLink = document.getElementById(`toggle-description-${counter}`);
 
-    if (fullDesc.style.display === "none") {
-        fullDesc.style.display = "inline";
-        shortDesc.style.display = "none";
+    if (fullDescPro.style.display === "none") {
+        fullDescPro.style.display = "inline";
+        shortDescPro.style.display = "none";
         toggleLink.innerText = "Свернуть";
     } else {
-        fullDesc.style.display = "none";
-        shortDesc.style.display = "inline";
+        fullDescPro.style.display = "none";
+        shortDescPro.style.display = "inline";
         toggleLink.innerText = "Развернуть";
+    }
+}
+
+
+// subdivision_list.html -- Подразделения
+function toggleDescriptionSub(counter) {
+    const shortSub = document.getElementById(`sub-short-${counter}`);
+    const fullSub = document.getElementById(`sub-full-${counter}`);
+    const toggleSub = document.getElementById(`sub-toggle-${counter}`);
+
+    if (fullSub.style.display === "none") {
+        fullSub.style.display = "inline";
+        shortSub.style.display = "none";
+        toggleSub.innerText = "Свернуть";
+    } else {
+        fullSub.style.display = "none";
+        shortSub.style.display = "inline";
+        toggleSub.innerText = "Развернуть";
     }
 }
 
 
 // project_info.html
-function toggleDescription(counter) {
-    const shortDesc = document.getElementById(`sub-desc-short-${counter}`);
-    const fullDesc = document.getElementById(`sub-desc-full-${counter}`);
-    const toggleLink = document.getElementById(`toggle-desc-${counter}`);
+function toggleDescription() {
+    const shortDesc = document.getElementById('short-description');
+    const fullDesc = document.getElementById('full-description');
+    const toggleLink = document.getElementById('toggle-description');
 
     if (fullDesc.style.display === "none") {
         fullDesc.style.display = "inline";
@@ -50,6 +68,26 @@ function toggleDescription(counter) {
         toggleLink.innerText = "Развернуть";
     }
 }
+
+// project_list.html
+function toggleDescriptionProd(counter) {
+    const shortDescProd = document.getElementById(`short-desc-${counter}`);
+    const fullDescProd = document.getElementById(`full-desc-${counter}`);
+    const toggleLinkProd = document.getElementById(`toggle-desc-${counter}`);
+
+    if (fullDescProd.style.display === "none") {
+        fullDescProd.style.display = "inline";
+        shortDescProd.style.display = "none";
+        toggleLinkProd.innerText = "Свернуть";
+    } else {
+        fullDescProd.style.display = "none";
+        shortDescProd.style.display = "inline";
+        toggleLinkProd.innerText = "Развернуть";
+    }
+}
+
+
+
 
 
 function toggleInfo() {
