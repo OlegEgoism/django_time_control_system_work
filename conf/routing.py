6c1e8caba7481f031cbd0225aeea62d59c9d5d8c
OlegEgoism <olegpustovalov220@gmail.com>
2024-11-10 20:46:00 +0300

    Файл грузит, но есть баги.

--- conf/routing.py
+++ conf/routing.py
@@ -4,5 +4,6 @@ from .consumers import ChatConsumer
 websocket_urlpatterns = [
     path("<room_slug>", ChatConsumer.as_asgi()),
     path("ws/<str:room_slug>/", ChatConsumer.as_asgi()),
+    path('ws/<room_slug>/', ChatConsumer.as_asgi()),
 ]
 
